ebda6d0e390a966902d0d05b51fb9a303a97a01d
Emilie SERRE <emilieserre@hotmail.com>
2019-06-06 21:38:44 +0200

    add datetime for checkin

--- src/Controller/CheckinDateController.php
+++ src/Controller/CheckinDateController.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CheckinDateController extends AbstractController
+{
+    /**
+     * @Route("/checkin/date", name="checkin_date")
+     */
+    public function index()
+    {
+        return $this->render('checkin_date/index.html.twig', [
+            'controller_name' => 'CheckinDateController',
+        ]);
+    }
+}
