0a7ea65d736facfb4a68aa5f4863124e12db655e
Emilie SERRE <emilieserre@hotmail.com>
2019-06-07 13:10:23 +0200

    sub consol.log

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,5 +11,4 @@ require('../scss/app.scss');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 import './checkindate';
