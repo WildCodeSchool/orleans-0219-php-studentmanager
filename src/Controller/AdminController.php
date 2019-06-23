72ce98aa0a372f7abdca142d259e74a4be4c2cf2
Guiderdoni Bastien <beegeebuzz@hotmail.fr>
2019-06-23 17:00:22 +0200

    create overview page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,33 @@
 
 namespace App\Controller;
 
+use App\Controller\StudentController;
+use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin", name="admin")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="_index")
      */
     public function index()
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/overview", name="_overview", methods={"GET"})
+     */
+    public function overview(StudentRepository $studentRepository)
+    {
+        return $this->render('admin/overview.html.twig', [
+            'students' => $studentRepository->findAll(),
         ]);
     }
 }
