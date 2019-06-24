c30169d24b1620d20dafe28fd822b1d51f803853
Guiderdoni Bastien <beegeebuzz@hotmail.fr>
2019-06-24 15:59:45 +0200

    correction

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,10 +1,8 @@
 <?php
 
 namespace App\Controller;
-
 use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -13,17 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/", name="_index")
-     */
-    public function index(StudentRepository $studentRepository): Response
-    {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
-    }
-
-    /**
-     * @Route("/overview", name="_overview", methods={"GET"})
+     * @Route("/overview", name="_overview")
      */
     public function overview(StudentRepository $studentRepository)
     {
--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -16,42 +16,8 @@ class Admin
      */
     private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $Firstname;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $lastname;
-
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getFirstname(): ?string
-    {
-        return $this->Firstname;
-    }
-
-    public function setFirstname(string $Firstname): self
-    {
-        $this->Firstname = $Firstname;
-
-        return $this;
-    }
-
-    public function getLastname(): ?string
-    {
-        return $this->lastname;
-    }
-
-    public function setLastname(string $lastname): self
-    {
-        $this->lastname = $lastname;
-
-        return $this;
     }
 }
