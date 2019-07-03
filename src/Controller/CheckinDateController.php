82869ac1ff5324b2b299b92a590a9a273beb515b
Emilie SERRE <emilieserre@hotmail.com>
2019-07-03 15:09:53 +0200

    on air

--- src/Controller/CheckinDateController.php
+++ src/Controller/CheckinDateController.php
@@ -48,8 +48,7 @@ class CheckinDateController extends AbstractController
                     'danger',
                     'Vous avez déjà signalé votre présence cet après-midi, bonne sieste !'
                 );
-            } elseif ($dateService->isCheckinAllowed($presence->getDate())
-            ) {
+            } elseif ($dateService->isCheckinAllowed($presence->getDate())) {
                 $entityManager->persist($presence);
                 $entityManager->flush();
                 $this->addFlash(
