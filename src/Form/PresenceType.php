4ac6e79fe11b34d0f8302e8a6cda249e3f77bfaf
Emilie SERRE <emilieserre@hotmail.com>
2019-07-05 09:46:07 +0200

    crud presence on air

--- src/Form/PresenceType.php
+++ src/Form/PresenceType.php
@@ -3,7 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Presence;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,6 +17,12 @@ class PresenceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder
+            ->add('date', DateType::class, ['label' => 'Date'])
+            ->add('user', EntityType::class, [
+                'class' => PresenceType::class,
+                'choice_label' => 'username',
+                'label' => 'User']);
     }
 
     public function configureOptions(OptionsResolver $resolver)
